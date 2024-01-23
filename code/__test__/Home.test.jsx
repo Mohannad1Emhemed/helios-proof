2c7d3f6e0a8cd6c719f13d638eecaa32ebca105b
Mohannad1Emhemed <mohaned1alrojpany@gmail.com>
2024-01-23 17:34:30 +0000

    failing test

--- code/__test__/Home.test.jsx
+++ code/__test__/Home.test.jsx
@@ -18,4 +18,13 @@ describe('Home', () => {
         expect(getElms[0]).toBeInTheDocument(); // Use getElms[0] to check the first matching element
         
     });
+
+    it('should containe the text "Lorem ipsum"', () => {
+        render(<Home />);
+
+        const getElms = screen.getAllByText(/Hello Alpha Corp/i);
+        expect(getElms.length).toBeGreaterThan(0);
+        expect(getElms[0]).toBeInTheDocument(); // Use getElms[0] to check the first matching element
+        
+    });
 });
