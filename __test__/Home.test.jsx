042e8bc49d6ea312222c6666a0ddca92fca32cec
Mohannad1Emhemed <mohaned1alrojpany@gmail.com>
2024-01-23 10:37:20 +0000

    add test

---
+++ __test__/Home.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+import Home from '../pages/index'
+
+describe('Home', () => {
+   it('should have About text', () => {
+        render(<Home />);
+
+        const getElm = screen.getByText('About');
+
+        expect(getElm).toBeInTheDocument();
+    });
+
+    it('should containe the text "Lorem ipsum"', () => {
+        render(<Home />);
+
+        const getElms = screen.getAllByText(/Welcome to Alpha Corp/i);
+        expect(getElms.length).toBeGreaterThan(0);
+        expect(getElms[0]).toBeInTheDocument(); // Use getElms[0] to check the first matching element
+        
+    });
+});
